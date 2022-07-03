9a6718263d0a36f5aaf3ab29cc360308f1bcec8a
Double Young <yang5.yang@intel.com>
2022-07-03 16:02:06 +0800

    add docker build proxy, CUDA pub key update, debug env and scripts

--- rnn_speech_recognition/pytorch/scripts/docker/build.sh
+++ rnn_speech_recognition/pytorch/scripts/docker/build.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-docker build . --rm -t mlperf/rnn_speech_recognition
+docker build . --rm -t mlperf/rnn_speech_recognition \
+	--build-arg http_proxy="http://mlp-sdp-icx-1046.jf.intel.com:44333" \
+	--build-arg https_proxy="http://mlp-sdp-icx-1046.jf.intel.com:44333"
