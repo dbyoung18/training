befa59a053bc8de645384aa2710812e11267c9c3
Double Young <yang5.yang@intel.com>
2023-03-13 16:06:22 +0800

    fix proxy issue

--- rnn_speech_recognition/pytorch/scripts/docker/build.sh
+++ rnn_speech_recognition/pytorch/scripts/docker/build.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2004/x86_64/cuda-keyring_1.0-1_all.deb
+
 docker build . --rm -t mlperf/rnn_speech_recognition \
-	--build-arg http_proxy="http://mlp-sdp-icx-1046.jf.intel.com:44333" \
-	--build-arg https_proxy="http://mlp-sdp-icx-1046.jf.intel.com:44333"
+	--build-arg http_proxy=${http_proxy} \
+	--build-arg https_proxy=${https_proxy}
